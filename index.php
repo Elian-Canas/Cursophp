83910ef4bbec06b16d2316fd400d2d6c2c1454f4
Elian <eliansa123@gmail.com>
2023-10-22 11:21:27 -0500

    Argumentos fijos y variables en funciones explicado

--- index.php
+++ index.php
@@ -4,7 +4,21 @@ function valoracion($nombre, $rating = 5) {
     echo "El $nombre tiene un rating de $rating";
 }
 
-valoracion("Curso PHP 8 desde cero")
 
 //Cuando algun parametro es opcional y queremos tener uno por defecto esto es lo que podemos hacer.
+
+function concatenar(...$palabras) {
+    $resultado = "";
+    foreach($palabras as $palabra) {
+        
+        $resultado .= $palabra . " ";
+
+    }
+    echo $resultado;
+}
+
+concatenar('Curso', 'PHP', '8','Desde', 'Cero');
+
+//Esto se puede hacer cuando no sabemos cuantos parametros vamos a necesitar en una funcion, al poner ...$palabras, todos los parametros que pongamos se guardaran en un array. 
+
 ?>
