b5e93f1feccb7eb528f6d1a83ee3cfa9738685d4
Elian <eliansa123@gmail.com>
2023-10-24 11:04:27 -0500

    Como crear formularios con PHP y guardar datos

--- index.php
+++ index.php
@@ -1,24 +1,77 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action= "procesar.php" method= "POST" enctype="multipart/form-data" >
+        <label>
+            Nombre:
+            <input type="text" name= "name">
+        </label>
+    <br>
+    <br>
+        <label>
+            Edad:
+            <input type="number" name= "edad">
+        </label>
+    <br>
+    <p>Sexo:</p>
+<select name= "sexo">
+    <option value="Masculino">Masculino</option>
+    <option value="Femenino">Femenino</option>
+</select>
 
-// $array = [1, 2, 3];
+    <!-- <label>
+            
+            <input type="radio" name= "sexo" value="Masculino">
+            Masculino
+        </label>
+        <label>
+            
+            <input type="radio" name= "sexo" value="Femenino">
+            Femenino
+        </label> -->
+        <br>
+        <br>
+        <p>Roles:</p>
+        <label>
+            
+            <input type="checkbox" name= "roles[]" value="Administrador">
+            Administrador
+        </label>
+    
+        <label>
+            
+            <input type="checkbox" name= "roles[]" value="Editor">
+            Editor
+        </label>
+        
+        <label>
+            
+            <input type="checkbox" name= "roles[]" value="Moderador">
+            Moderador
+        </label>
+        <br>
+        <br>
+        <label>
+            Imagen:
+            <br>
+            <input type="file" name="image">
+        </label>
+    
+        <br>
+        <br>
+<label>Mensaje</label>
+<br>
+<textarea name= "mensaje" cols="30" rows="10"></textarea>
 
-// list($a, $b, $c) = $array;
+        <br>
+        <button type="text">Enviar</button>
+        
 
-// echo $b . "<br>";
-
-// $array2= range(10,20);
-
-// var_dump($array2);
-// echo count($array2);
-
-$array = ['Victor', 'Pamela', 'Elizabeth', 'Iris'];
-
-unset($array[2]);
-// unset sirve para borrar un elemento del array
-
-if (in_array('Elizabeth', $array)) {
-    echo "El valor buscado se encuentra dentro de la lista";
-} else {
-    echo "El valor buscado NO se encuentra dentro de la lista";
-}
-
+    </form>
+</body>
+</html>
