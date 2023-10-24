efdc5c57f27b96c5881bf8eb9a79c8a9c668ec7a
Elian <eliansa123@gmail.com>
2023-10-24 11:06:22 -0500

    Como crear formularios con PHP y guardar

---
+++ procesar.php
@@ -0,0 +1,25 @@
+<?php
+$nombre = $_REQUEST['name'];
+$edad = $_REQUEST['edad'];
+$sexo = $_REQUEST['sexo'];
+$roles = $_REQUEST['roles'];
+$mensaje = $_REQUEST['mensaje'];
+
+$image = $_FILES['image'];
+$patch = $_SERVER['DOCUMENT_ROOT'] . '/miProyectoPHP/Imagenes' . '/' . $image['name'];
+
+echo "<p>El nombre del usuario es: $nombre </p>";
+echo "<p>La edad del usuario es: $edad </p>";
+echo "<p>El sexo del usuario es: $sexo </p>";
+echo "<p>Roles: </p>";
+
+echo "<ul>";
+foreach($roles as $rol){
+    echo "<li>$rol</li>";
+}
+echo "</ul>";
+
+move_uploaded_file($image['tmp_name'], $patch);
+
+echo "<p>Mensaje Enviado</p>";
+echo "<p>$mensaje</p>";
